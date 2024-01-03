9f79da9fe070461734e5f395296b0e8af228347e
Namindu <namindu@live.com>
2024-01-03 10:49:30 +0530

    Namindu 1

---
+++ Online Banking System/Online Banking System.cpp
@@ -0,0 +1,12 @@
+﻿// Online Banking System.cpp : Defines the entry point for the application.
+//
+
+#include "Online Banking System.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
